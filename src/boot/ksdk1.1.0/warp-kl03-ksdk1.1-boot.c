e21ff29e27fcc8d85f19c317f047e51cbb322360
Adam Ashing <adam.ashing@gmail.com>
2020-12-17 20:27:11 +0000

    Averaging

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1360,18 +1360,22 @@ main(void)
 int light_percent1 = readlight() * 0.143;
 int light_percent2 = light_percent1;
 int temp_percent1 = (readTemp()-14) * 6.25;
-int temp_percent2 = temp_percent1;
 
 int i;
 for(i=1;i<100;++i)
 {
 	OSA_TimeDelay(1000);
+	int j;
+	int average_temp = 0;
+	for(j=1; j<10; ++j){
+		average_temp = average_temp + (readTemp()-14) * 6.25;
+	}
+	average_temp = average_temp * 0.1;
 	drawaline_yellow(i,i+1,light_percent1,light_percent2);
-	drawaline_red(i,i+1,temp_percent1,temp_percent2);
+	drawaline_red(i,i+1,temp_percent1,average_temp);
 	light_percent1 = light_percent2;
 	light_percent2 = readlight() * 0.1;
-	temp_percent1 = temp_percent2;
-	temp_percent2 = (readTemp()-14) * 6.25;
+	temp_percent1 = average_temp;
 	
 
 	}
