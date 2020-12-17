af5a45bfab0afae82322c36f15587da75793c94f
Adam Ashing <adam.ashing@gmail.com>
2020-12-17 22:33:55 +0000

    .

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1365,7 +1365,7 @@ int moisture_percent1 = (readMoisture()-320)*0.1429;
 int moisture_percent2 = moisture_percent1;
 int i;
 int user_input;
-SEGGER_RTT_WriteString(0, "Enter number of minutes (3 digits): ");
+SEGGER_RTT_printf(0, "\nEnter number of minutes required (3 digits) \n");
 for(i=1;i<100;++i)
 {
 	OSA_TimeDelay(1000);
